b920bd5d33184e74458a48ad37fdde26806d6d23
radubaciuDell <radumbaciu@gmail.com>
2022-10-16 15:50:50 +0300

    Add existing project files to Git

---
+++ src/main/java/com/arbor/arborvisualbackend/ArborvisualBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arbor.arborvisualbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArborvisualBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArborvisualBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arbor/arborvisualbackend/ArborvisualBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbor.arborvisualbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArborvisualBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
